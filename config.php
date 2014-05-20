36506219015365b445a18c748d7af46c963f6914
SamuelMoraesF <samuelmoraesf@gmail.com>
2014-05-20 19:44:37 -0300

    Incorporando configuração notas no config.php

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
 
-define('NOMEEVENTO', 'II Fórum Tchelinux de SM'); //Nome da palestra/evento a ser exibido no titulo da página
+define('NOMEEVENTO', 'Nome do Evento'); //Nome da palestra/evento a ser exibido no titulo da página
 
 define('FULLURL','http://localhost/AskSystem'); //Endereço completo da raiz do sistema de perguntas sem / no final
 
-define('PALESTRANTE','Fulano de Tales'); //Nome do palestrante
+define('PALESTRANTE','Fulano de Tal'); //Nome do palestrante
 
 define('SENDMETHOD','sqlite'); //Método de envio da pergunta, as opções podem ser 'email', 'pushover' ou 'sqlite'
 
 
 	// SE SENDMETHOD FOR EMAIL
 
-	define('SENDMETHODMAIL','samuelmoraesf@gmail.com'); //Defina aqui o endereço para o qual deverá ser enviado
+	define('SENDMETHODMAIL','email@exemplo.com'); //Defina aqui o endereço para o qual deverá ser enviado
 
 	define('SENDMETHODFROM','asksystem@samuelmoraesf.tk'); //Endereço de email que o PHP usará para enviar as mensagens
 
@@ -41,14 +41,10 @@ define('SENDMETHOD','sqlite'); //Método de envio da pergunta, as opções podem
 
 	define('ENABLEEMAILNOTIFY', true); //Habilita o envio de perguntas através de emails para o palestrante 
 
-		define('MAILNOTIFY','samuelmoraesf@gmail.com'); //Email a receber a mensagem
+		define('MAILNOTIFY','email@exemplo.com'); //Email a receber a mensagem
 
-		define('FROMNOTIFY','asksystem@samuelmoraesf.tk'); //Email a enviar a mensagem
+		define('FROMNOTIFY','email@exemplo.com'); //Email a enviar a mensagem
 
 $shownotas = true;
-
-$notas = "Aqui vai umas notas interessantes que podem ser úteis para enrolar os visitantes quando tem tempo de sobra. Posso pedir para enviarem alguns dados para a NSA com um link para o http://google.com.
-          -> Item1
-          -> Item2"
 
 ?>
